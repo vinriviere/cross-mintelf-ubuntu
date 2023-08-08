6c9421113552b1a00b920aea621c5fe1b37d6293
Vincent Rivière <vincent.riviere@freesbee.fr>
2023-08-08 16:09:12 +0200

    Rebuild mintbin

--- .github/main.sh
+++ .github/main.sh
@@ -22,7 +22,7 @@ echorun . .github/init_ppa.sh ppa:vriviere/mintelf
 #echorun .github/deploy_changed_packages.sh -m68k-atari-mintelf
 
 # The lines below are sorted according to dependencies
-echorun .github/deploy_ppa_all_dists.sh binutils-m68k-atari-mintelf
+#echorun .github/deploy_ppa_all_dists.sh binutils-m68k-atari-mintelf
 echorun .github/deploy_ppa_all_dists.sh mintbin-m68k-atari-mintelf
 
 #echorun .github/deploy_ppa_all_dists.sh gcc-m68k-atari-mintelf
