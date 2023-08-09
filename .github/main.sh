d454b75922121d1377433b8176d71793ec0af1fe
Vincent Rivière <vincent.riviere@freesbee.fr>
2023-08-09 16:55:20 +0200

    Build gemlib

--- .github/main.sh
+++ .github/main.sh
@@ -29,9 +29,9 @@ echorun . .github/init_ppa.sh ppa:vriviere/mintelf
 
 #echorun .github/deploy_ppa_all_dists.sh mintlib-m68k-atari-mintelf
 
-echorun .github/deploy_ppa_all_dists.sh fdlibm-m68k-atari-mintelf
+#echorun .github/deploy_ppa_all_dists.sh fdlibm-m68k-atari-mintelf
 
-#echorun .github/deploy_ppa_all_dists.sh gemlib-m68k-atari-mintelf
+echorun .github/deploy_ppa_all_dists.sh gemlib-m68k-atari-mintelf
 #echorun .github/deploy_ppa_all_dists.sh ncurses-m68k-atari-mintelf
 #echorun .github/deploy_ppa_all_dists.sh zlib-m68k-atari-mintelf
 
